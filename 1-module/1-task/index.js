c3d22429e18ffcc7efda4f7d9fd3e462c34e0d15
Andrei T <andrey7titov@gmail.com>
2022-11-22 16:58:41 +0300

    Module 1. Task 1. Updated.

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -2,13 +2,12 @@ function factorial(n) {
     // ваш код...
     let sum = 1;
 
-    if (n == 0 || n == 1) {
-        return sum;
-    } else {
-        for (let i = n; i >= 1; i--) {
-            sum = sum * i;
-        }
-        return sum;
+    if (n == 0 || n == 1) return sum;
+
+    for (let i = n; i >= 1; i--) {
+        sum = sum * i;
     }
+
+    return sum;
   
 }
